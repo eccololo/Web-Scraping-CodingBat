c3d11be5d34de937864d615d0bde043ece3c1187
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2023-04-25 13:31:29 +0200

    Adding spider time execution time calculating code.

--- spider.py
+++ spider.py
@@ -3,3 +3,9 @@ import requests
 import json
 from fake_useragent import UserAgent
 import time
+
+start = time.time()
+base_url = "https://codingbat.com"
+
+end = time.time()
+print(f"Scraping process finished succesfully in {round(end - start, 2)} seconds!")
